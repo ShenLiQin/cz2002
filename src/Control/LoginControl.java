f445e931d3f2578548460272584c43a75ee0b015
RichardYCX <feavfas@gmail.com>
2020-11-18 21:44:06 +0800

    added masking for password

--- src/Control/LoginControl.java
+++ src/Control/LoginControl.java
@@ -1,8 +1,11 @@
 package Control;
 
 import DataAccessObject.IUserDataAccessObject;
+import Helper.PasswordStorage;
 import ValueObject.AbstractUser;
 
+import java.io.Console;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class LoginControl {
@@ -20,19 +23,24 @@ public class LoginControl {
                 System.out.print("enter username: ");
                 String username = _scanner.nextLine();
 
-//            Console console = System.console();
-//            String password = Arrays.toString(console.readPassword());
-                System.out.print("enter password: ");
-                String password = _scanner.nextLine();
+//                System.out.println("Enter peer password:");
+//                String passwordArray = _scanner.nextLine();
 
-                user = userDataAccessObject.authenticate(username, password);
+                Console console = System.console();
+                if (console == null) {
+                    System.out.println("Couldn't get Console instance");
+                    System.exit(0);
+                }
+                String passwordArray = Arrays.toString(console.readPassword("Enter your password: "));
+                user = userDataAccessObject.authenticate(username, passwordArray);
                 if (user == null) {
                     System.out.println("wrong username/password");
                 }
             } while (user == null);
-
-        } catch (Exception e) {
-        System.out.println("error");
+        } catch (PasswordStorage.CannotPerformOperationException e) {
+            System.out.println("error validating password");
+        } catch (PasswordStorage.InvalidHashException e) {
+            System.out.println("error unhashing password");
         }
         return user;
     }
